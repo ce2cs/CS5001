582365e3e0bf847253a10ce7884c8f2713f99201
ce2cs <boyang.gao@yahoo.com>
2021-09-16 17:30:46 -0700

    fix lab2 format

--- improper_fractions.py
+++ improper_fractions.py
@@ -1,6 +1,6 @@
 def main():
     try:
-        n = float(input("Enter a floating point number: "))
+        n = float(input("Enter a floating point number:"))
         numerator = int(100 * n)
     except ValueError as e:
         print("Please enter a floating number")
