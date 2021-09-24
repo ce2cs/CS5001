542b8edb5e76dca9cb13c42766aa3dcda2d714b0
ce2cs <boyang.gao@yahoo.com>
2021-09-24 16:19:48 -0700

    feature: finished lab 3

---
+++ number_even.py
@@ -0,0 +1,10 @@
+def main():
+    number = int(input("Input number:"))
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+        print(number, "is odd")
+
+
+if __name__ == '__main__':
+    main()
---
+++ sorting_numbers.py
@@ -0,0 +1,22 @@
+def main():
+    first_number = float(input("Input first number:"))
+    second_number = float(input("Input second number:"))
+    third_number = float(input("Input third number:"))
+    if first_number <= second_number:
+        if second_number <= third_number:
+            print(first_number, second_number, third_number)
+        elif third_number <= first_number:
+            print(third_number, first_number, second_number)
+        else:
+            print(first_number, third_number, second_number)
+    else:
+        if first_number <= third_number:
+            print(second_number, first_number, third_number)
+        elif third_number <= second_number:
+            print(third_number, second_number, first_number)
+        else:
+            print(second_number, third_number, second_number)
+
+
+if __name__ == '__main__':
+    main()
