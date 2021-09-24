875dbe6182ba4f4d261bc280aaaeb6e0f1ffcddd
ce2cs <boyang.gao@yahoo.com>
2021-09-24 16:46:12 -0700

    fix: space format

--- sorting_numbers.py
+++ sorting_numbers.py
@@ -4,18 +4,19 @@ def main():
     third_number = float(input("Input third number: "))
     if first_number <= second_number:
         if second_number <= third_number:
-            print(first_number, ",", second_number, ",", third_number)
+            sorted_numbers = [first_number, second_number, third_number]
         elif third_number <= first_number:
-            print(third_number, ",", first_number, ",", second_number)
+            sorted_numbers = [third_number, first_number, second_number]
         else:
-            print(first_number, ",", third_number, ",", second_number)
+            sorted_numbers = [first_number, third_number, second_number]
     else:
         if first_number <= third_number:
-            print(second_number, ",", first_number, ",", third_number)
+            sorted_numbers = [second_number, first_number, third_number]
         elif third_number <= second_number:
-            print(third_number, ",", second_number, ",", first_number)
+            sorted_numbers = [third_number, second_number, first_number]
         else:
-            print(second_number, ",", third_number, ",", second_number)
+            sorted_numbers = [second_number, third_number, second_number]
+    print(", ".join([str(n) for n in sorted_numbers]))
 
 
 if __name__ == '__main__':
