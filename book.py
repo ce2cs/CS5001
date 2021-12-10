d1995fadda0374333a9785ac5605281655f28e1c
ce2cs <boyang.gao@yahoo.com>
2021-12-09 23:33:09 -0800

    feature: first attempt

--- book.py
+++ book.py
@@ -1,13 +1,33 @@
+import os
+
 from readability import *
 
 
 class Book:
     def __init__(self, isbn, title, author, publish_year, file_format, resource_path):
+        isbn = str(isbn)
+        if len(isbn) != 13 or \
+                not (isbn.startswith("978") or isbn.startswith("979")) or \
+                not isbn.isnumeric():
+            raise ValueError("ISBN format check failed:"
+                             "Please input a 13-digit number starting "
+                             "in \"978\" or \"979\"")
         self._isbn = isbn
+        if type(title) != str:
+            raise ValueError("Title must be a string")
         self._title = title
+        if type(author) != str:
+            raise ValueError("Author must be a string")
         self._author = author
+        if type(publish_year) != int or publish_year > 2022:
+            raise ValueError("publish year should be a valid year in Integer")
         self._publish_year = publish_year
+        if file_format not in ["Hardcover", "Softcover", "Kindle", "PDF"]:
+            raise ValueError("file format must be Hardcover, "
+                             "Softcover, Kindle, or PDF")
         self._format = file_format
+        if not os.path.exists(resource_path):
+            raise ValueError("resource file not exist!")
         self._resource_path = resource_path
 
     def get_title(self):
@@ -55,30 +75,35 @@ class Book:
     def get_index(self):
         opened_file = self._open_file()
         words_count = {}
-        hythen_breaked_word_first_half = ""
+        hyphen_break_word_first_half = ""
         for line in opened_file.readlines():
-            word = hythen_breaked_word_first_half
-            hythen_breaked_word_first_half = ""
+            word = hyphen_break_word_first_half
+            hyphen_break_word_first_half = ""
             for i in range(len(line)):
                 if line[i].isalnum():
                     word += line[i]
                 elif line[i] == '-' and i == len(line) - 1:
-                    hythen_breaked_word_first_half = word
+                    hyphen_break_word_first_half = word
+                    word = ""
                 elif line[i] == '-':
                     word += line[i]
                 elif len(word) > 0:
                     words_count[word] = words_count.setdefault(word, 0) + 1
                     word = ""
+            if len(word) > 0:
+                words_count[word] = words_count.setdefault(word, 0) + 1
+                word = ""
+
         opened_file.close()
         return words_count
 
     def __str__(self):
         print([self.get_title() + " by " + self.get_author(),
-                          str(self.get_year()) + "\nISBN: " + str(self._isbn),
-                          self.get_format(),
-                          str(self.get_readability_grade()),
-                          self.get_filename()
-                          ])
+               str(self.get_year()) + "\nISBN: " + str(self._isbn),
+               self.get_format(),
+               str(self.get_readability_grade()),
+               self.get_filename()
+               ])
         return ", ".join([self.get_title() + " by " + self.get_author(),
                           str(self.get_year()) + "\nISBN: " + str(self._isbn),
                           self.get_format(),
