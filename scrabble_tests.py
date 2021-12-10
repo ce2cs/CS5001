b8375de19219a088d12538ca93b4f8aeec3a875c
ce2cs <boyang.gao@yahoo.com>
2021-12-09 21:47:48 -0800

    fix: style

--- scrabble_tests.py
+++ scrabble_tests.py
@@ -12,7 +12,8 @@ class ScrabbleTest(unittest.TestCase):
         words = []
         for i in range(100):
             length = random.randint(1, 10)
-            word = ''.join([chr(ord('a') + random.randint(0, 25)) for _ in range(length)])
+            word = ''.join([chr(ord('a') + random.randint(0, 25))
+                            for _ in range(length)])
             words.append(word)
             expected[length].append(word)
         self.assertEqual(expected, store_words(words))
@@ -22,7 +23,8 @@ class ScrabbleTest(unittest.TestCase):
         actual = []
         for i in range(100):
             length = random.randint(1, 10)
-            word = ''.join([chr(ord('a') + random.randint(0, 25)) for _ in range(length)])
+            word = ''.join([chr(ord('a') + random.randint(0, 25))
+                            for _ in range(length)])
             words_dict[length].append(word)
             if 'a' in word and 'b' in word and length <= 6:
                 actual.append(word)
@@ -34,14 +36,16 @@ class ScrabbleTest(unittest.TestCase):
     def test_get_scores(self):
         expected = collections.defaultdict(list)
         words = []
+        character_values = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1,
+                            'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8,
+                            'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
+                            'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
+                            'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
         for i in range(100):
             length = random.randint(1, 10)
-            word = ''.join([chr(ord('a') + random.randint(0, 25)) for _ in range(length)])
+            word = ''.join([chr(ord('a') + random.randint(0, 25))
+                            for _ in range(length)])
             words.append(word)
-            character_values = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2,
-                                'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1,
-                                'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
-                                'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
             score = 0
             for c in word:
                 score += character_values[c]
