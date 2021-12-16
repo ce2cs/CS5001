3f454bb4dbe176af0289d4d3b09a06bbea129f6a
ce2cs <boyang.gao@yahoo.com>
2021-12-15 22:43:34 -0800

    fix: style

--- gradebook_tests.py
+++ gradebook_tests.py
@@ -16,6 +16,7 @@ class TestGradebook(TestCase):
     """
     Test case of class Gradebook
     """
+
     def setUp(self):
         """
         every before time a test runs, prepare a random grade book
@@ -25,8 +26,10 @@ class TestGradebook(TestCase):
         self.students = []
         self.students_number = random.randint(10, 100)
         for _ in range(self.students_number):
-            self.students.append(''.join([chr(ord('a') + random.randint(0, 25))
-                                          for _ in range(random.randint(1, 10))]))
+            self.students.append(''.join(
+                [chr(ord('a') + random.randint(0, 25))
+                 for _ in range(random.randint(1, 10))])
+            )
 
         # prepare a grade book
         parameters = ["CS5001", 2021, "Fall", "John Wilder", self.students]
@@ -108,7 +111,9 @@ class TestGradebook(TestCase):
         sum_of_grade = 0
         for student in self.grades:
             sum_of_grade += sum(self.grades[student])
-        actual_average_grade = sum_of_grade / self.students_number * self.assignments_number
+        actual_average_grade = sum_of_grade / (
+                self.students_number * self.assignments_number
+        )
         # the average grades are float, need to allow minor error
         self.assertAlmostEqual(self.gb.average_grade(), actual_average_grade)
 
@@ -140,10 +145,12 @@ class TestGradebook(TestCase):
         actual_letter_grade = {}
 
         for student in self.students:
-            student_avg_grade = sum(self.grades[student]) / self.assignments_number
+            student_avg_grade = sum(self.grades[student]) / \
+                                self.assignments_number
             for grade_threshold, letter_grade in grade_scale_reference:
                 if student_avg_grade > grade_threshold:
                     actual_letter_grade[student] = letter_grade
                     break
 
-        self.assertEqual(self.gb.letter_grade(self.assignments_number), actual_letter_grade)
+        self.assertEqual(self.gb.letter_grade(self.assignments_number),
+                         actual_letter_grade)
