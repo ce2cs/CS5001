5248aec56787d720a34651ba663607ca966ba4b5
ce2cs <boyang.gao@yahoo.com>
2021-12-15 22:39:13 -0800

    feature: finished

--- gradebook_tests.py
+++ gradebook_tests.py
@@ -5,3 +5,145 @@ the methods described in gradebook.py
 You do not need to implement the methods, the description given
 in the comments are sufficient for creating tests
 '''
+import collections
+import random
+from unittest import TestCase
+
+from gradebook import Gradebook
+
+
+class TestGradebook(TestCase):
+    """
+    Test case of class Gradebook
+    """
+    def setUp(self):
+        """
+        every before time a test runs, prepare a random grade book
+        :return: None
+        """
+        # prepare students
+        self.students = []
+        self.students_number = random.randint(10, 100)
+        for _ in range(self.students_number):
+            self.students.append(''.join([chr(ord('a') + random.randint(0, 25))
+                                          for _ in range(random.randint(1, 10))]))
+
+        # prepare a grade book
+        parameters = ["CS5001", 2021, "Fall", "John Wilder", self.students]
+        self.gb = Gradebook(*parameters)
+
+        # prepare grades
+        self.grades = collections.defaultdict(list)
+        self.assignments_number = random.randint(5, 10)
+        for student in self.students:
+            assignments_grades = {}
+            for _ in range(self.assignments_number):
+                grade = random.randint(60, 100)
+                assignments_grades[student] = grade
+                self.grades[student].append(grade)
+            # add assignment grades to grade book
+            self.gb.add_grade(assignments_grades)
+
+    def test_constructor(self):
+        """
+        Test functionality of constructor
+        :return: None
+        """
+        parameters = ["CS5001", 2021, "Fall", "John Wilder", ["Boyang Gao", ]]
+
+        # test functionality of constructor
+        try:
+            gb = Gradebook(*parameters)
+        except Exception as e:
+            self.assertTrue(False)
+
+        # test wrong course name type
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[0] = 5001
+            gb = Gradebook(*wrong_parameters)
+
+        # test wrong year type
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[1] = "2021"
+            gb = Gradebook(*wrong_parameters)
+
+        # test year less than 1999
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[1] = 1999
+            gb = Gradebook(*wrong_parameters)
+
+        # test wrong semester value
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[2] = "Winter"
+            gb = Gradebook(*wrong_parameters)
+
+        # test wrong instructor type
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[3] = ["John Wilder"]
+            gb = Gradebook(*wrong_parameters)
+
+        # test wrong students type (string)
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[4] = "Boyang Gao"
+            gb = Gradebook(*wrong_parameters)
+
+        # test wrong students type (list of types other than string)
+        with self.assertRaises(ValueError):
+            wrong_parameters = parameters[:]
+            wrong_parameters[4] = [0, 1]
+            gb = Gradebook(*wrong_parameters)
+
+    def test_average_grade(self):
+        """
+        Test correctness of average_grade method of Gradebook
+        :return: None
+        """
+        # get actual average grade not replying on any method in grade book
+        sum_of_grade = 0
+        for student in self.grades:
+            sum_of_grade += sum(self.grades[student])
+        actual_average_grade = sum_of_grade / self.students_number * self.assignments_number
+        # the average grades are float, need to allow minor error
+        self.assertAlmostEqual(self.gb.average_grade(), actual_average_grade)
+
+    def test_letter_grade(self):
+        """
+        Test correctness of letter_grade method of Gradebook
+        :return: None
+        """
+        # test input parameter type error
+        with self.assertRaises(ValueError):
+            self.gb.letter_grade("10")
+        # test if the assignments number less than 1
+        with self.assertRaises(ValueError):
+            self.gb.letter_grade(0)
+
+        grade_scale_reference = [
+            (93, "A"),
+            (90, "A-"),
+            (86, "B+"),
+            (82, "B"),
+            (77, "B-"),
+            (73, "C+"),
+            (69, "C"),
+            (65, "C-"),
+            (0, "F")
+        ]
+
+        # get actual letter grade
+        actual_letter_grade = {}
+
+        for student in self.students:
+            student_avg_grade = sum(self.grades[student]) / self.assignments_number
+            for grade_threshold, letter_grade in grade_scale_reference:
+                if student_avg_grade > grade_threshold:
+                    actual_letter_grade[student] = letter_grade
+                    break
+
+        self.assertEqual(self.gb.letter_grade(self.assignments_number), actual_letter_grade)
--- recursion.py
+++ recursion.py
@@ -3,9 +3,16 @@ Write a recursive version of list_of_strings_loop
 Write a useful comment for your function
 '''
 import random
+import unittest
 
 
-def list_of_strings_loop(number_of_strings):
+def list_of_strings_recursion(number_of_strings):
+    """
+    generate list of random alphabetic strings recursively,
+    the length of list will equal to number_of_strings
+    :param number_of_strings: Integer
+    :return: random strings list
+    """
     if number_of_strings == 0:
         return []
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -14,10 +21,33 @@ def list_of_strings_loop(number_of_strings):
     this_word = ""
     for character in range(string_length):
         this_word += random.choice(alphabet)
+    # every time add generated random word to return list
     list_of_strings.append(this_word)
-    list_of_strings.extend(list_of_strings_loop(number_of_strings - 1))
+    # recursively call this function with input - 1
+    # add append the returned list to current return list
+    list_of_strings.extend(list_of_strings_recursion(number_of_strings - 1))
+    return list_of_strings
+
+
+def list_of_strings_loop(number_of_strings):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    list_of_strings = []
+    for word in range(number_of_strings):
+        string_length = random.randint(1, 5)
+        this_word = ""
+        for character in range(string_length):
+            this_word += random.choice(alphabet)
+        list_of_strings.append(this_word)
     return list_of_strings
 
 
 if __name__ == '__main__':
-    print(list_of_strings_loop(10))
+    # test the function list_of_strings_recursion
+    tc = unittest.TestCase()
+    # use random seed to control the generated random string
+    random.seed(0)
+    actual = list_of_strings_loop(10)
+    # remember to reset the random seed every time call
+    random.seed(0)
+    expected = list_of_strings_recursion(10)
+    tc.assertEqual(actual, expected)
